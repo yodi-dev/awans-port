a8e1e7dc2c92f2d5e2debffb3d05890b822b98ae
irawanyodi <dykhazta@gmail.com>
2024-08-25 21:06:31 +0700

    setup: navigation - done

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,20 @@ import { RouterLink, RouterView } from "vue-router";
     </div>
   </div>
   <RouterView />
+
+  <nav class="bottom-navbar">
+    <ul>
+      <li>
+        <a title="Home" href="/"><i class="fa-solid fa-house-chimney"></i></a>
+      </li>
+      <li>
+        <a title="News" href="/news"><i class="fa-solid fa-newspaper"></i></a>
+      </li>
+      <li>
+        <a title="Blog" href="/blog"><i class="fa-solid fa-blog"></i></a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped>
@@ -25,6 +39,43 @@ img {
   font-weight: 500;
   font-size: 2em;
   font-style: normal;
+}
+
+.bottom-navbar {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  border-radius: 6px 6px 0 0;
+  height: 9rem;
+  background-color: #7d8abc;
+  padding: 10px 0;
+  z-index: 1000;
+}
+.bottom-navbar ul {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.bottom-navbar ul li {
+  flex-grow: 1;
+  /* height: 2.5rem; */
+  text-align: center;
+  margin: 10px 0;
+  /* padding: 10px; */
+}
+.bottom-navbar ul li a {
+  text-decoration: none;
+  color: white;
+  font-weight: 600;
+  padding: 10px;
+}
+
+.bottom-navbar ul li a:hover {
+  color: #7d8abc;
+  background-color: #fff;
 }
 
 @media (min-width: 992px) {
---
+++ src/components/news.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>news</h1>
+</template>
